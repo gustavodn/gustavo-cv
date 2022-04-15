9bcdb1080b6f109dab3a1de6779dd4564b1dd648
Gustavo <gustavo4581@gmail.com>
2022-04-15 16:29:47 -0400

    update

--- js/main.js
+++ js/main.js
@@ -308,8 +308,9 @@
         $("#map").googleMap({
             zoom: 16 // Google Map ZOOM. You can change this value
         });
+
         $("#map").addMarker({
-            address: "S601 Townsend Street, San Francisco, California, USA", // Your Address. Change it
+            address: "Calle Peninsular, Punto Fijo 4102, Falcón, Venezuela", // Your Address. Change it
         });
     });
 
